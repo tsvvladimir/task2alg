81c2f7fe276bfcf9900dfb37376551f51c48df76
Vladimir Tsvetkov <tsvvladimir95@me.com>
2014-10-21 01:49:32 +0400

    add SOR, play with w and maxsteps

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello!");
-    //test Jacobi
+    public static void testJacobi() {
+        System.out.println("hello Jacobi!");
+        //test Jacobi
         int n = 5;
         int maxsteps = 25;
         //Matrix A = Matrix.randomsym(n, n);
@@ -16,6 +15,32 @@ public class Main {
         j.dsplit();
         Matrix result = j.execute(maxsteps);
         result.show();
-        System.out.println("bye bye!");
+        System.out.println("bye bye Jacobi!");
+
+    }
+
+    public static void testSOR() {
+        //test SOR
+        System.out.println("hello SOR!");
+        int n = 5;
+        int maxsteps = 100;
+        double w = 1.5;
+        //Matrix A = Matrix.randomsym(n, n);
+        Matrix A = new Matrix(new double[][] {{2.0, 1.0}, {5.0, 7.0}} );
+        Matrix help1 = new Matrix(n, 1);
+        for(int i = 0; i < n; i++)
+            help1.data[i][0] = 1;
+        //B = new Matrix(A.times(help1));
+        Matrix B = new Matrix(new double[][] {{11}, {13}});
+        SOR sor = new SOR(A, B);
+        sor.dsplit();
+        Matrix result = sor.execute(maxsteps, w);
+        result.show();
+        System.out.println("bye bye SOR!");
+    }
+    public static void main(String[] args) {
+	// write your code here
+     testJacobi();
+     testSOR();
     }
 }
---
+++ src/SOR.java
@@ -0,0 +1,65 @@
+/**
+ * Created by vladimirtsvetkov on 21/10/14.
+ */
+public class SOR {
+    Matrix A;
+    Matrix B;
+    Matrix D;
+    Matrix L;
+    Matrix U;
+    public SOR(Matrix A, Matrix B) {
+        this.A = A;
+        this.B = B;
+    }
+    public  SOR(Matrix D, Matrix L, Matrix U, Matrix B) {
+        this.D = D;
+        this.L = L;
+        this.U = U;
+        this.B = B;
+    }
+    public void dsplit() {
+        Matrix D = new Matrix(A.M, A.M);
+        Matrix L = new Matrix(A.M, A.M);
+        Matrix U = new Matrix(A.M, A.M);
+        for(int i = 0; i < A.M; i++) {
+            D.setElement(i, i, A.GetElement(i, i));
+        }
+        for(int i = 0; i < (A.M - 1); i++)
+            for(int j = i + 1; j < A.N; j++)
+                U.setElement(i, j, A.GetElement(i, j));
+        for(int i = 0; i < (A.M - 1); i++)
+            for(int j = i + 1; j < A.N; j++)
+                L.setElement(j, i, A.GetElement(j, i));
+        this.D = D;
+        this.L = L.muldig(-1);
+        this.U = U.muldig(-1);
+        System.out.println("A");
+        this.A.show();
+        System.out.println("D");
+        this.D.show();
+        System.out.println("L");
+        this.L.show();
+        System.out.println("U");
+        this.U.show();
+        System.out.println("Solution:");
+    }
+    public Matrix execute(int stepmax, double w) {
+        Matrix xk = new Matrix(D.M, 1);
+        Matrix xk1 = new Matrix(D.M, 1);
+        for(int i = 0; i < D.M; i++) {
+            xk1.setElement(i, 0, 1.0);
+        }
+        for(int step = 0; step < stepmax; step++) {
+            Matrix DminwL = D.minus(L.muldig(w));
+            Matrix DminwLinv = DminwL.degMin1();
+            Matrix help1 = D.muldig(1 - w).minus(U.muldig(w));
+            Matrix help2 = DminwLinv.times(help1.times(xk));
+            Matrix help3 = DminwLinv.muldig(w).times(B);
+            xk1 = help2.plus(help3);
+            xk = xk1;
+            xk.show();
+            System.out.println("\n");
+        }
+        return xk1;
+    }
+}
