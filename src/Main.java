f89dca8920685406a0dc530a64db899a712ca182
Vladimir Tsvetkov <tsvvladimir95@me.com>
2014-10-26 12:33:13 +0300

    changed tests

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
         int maxsteps = 25;
         //Matrix A = Matrix.randomsym(n, n);
         Matrix A = new Matrix(new double[][] {{2.0, 1.0}, {5.0, 7.0}} );
-        Matrix help1 = new Matrix(n, 1);
-        for(int i = 0; i < n; i++)
+        Matrix help1 = new Matrix(A.M, 1);
+        for(int i = 0; i < A.M; i++)
             help1.data[i][0] = 1;
-        //Matrix B = new Matrix(A.times(help1));
-        Matrix B = new Matrix(new double[][] {{11}, {13}});
+        Matrix B = new Matrix(A.times(help1));
+        //Matrix B = new Matrix(new double[][] {{11}, {13}});
         Jacobi j = new Jacobi(A, B);
         j.dsplit();
         Matrix result = j.execute(maxsteps);
@@ -43,7 +43,7 @@ public class Main {
     }
     public static void main(String[] args) {
 	// write your code here
-     //testJacobi();
-     testSOR();
+     testJacobi();
+     //testSOR();
     }
 }
